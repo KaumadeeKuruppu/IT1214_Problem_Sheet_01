99db4946d048b760d3133e896a0474b27d08dc3c
KaumadeeKuruppu <kaumadee16kuruppu@gmail.com>
2025-05-17 10:26:55 +0000

    01.java

--- 01.java
+++ 01.java
@@ -1,5 +1,13 @@
 class Student{
     public static void main(String[] args){
-        System.out.println("hello");
+        String name;
+        int studentId;
+        int daysAttend;
     }
+}
+
+Student(String name,int studentId,int daysAttend;){
+    this.name=name;
+    this studentId=studentId;
+    this daysAttend=daysAttend;
 }
